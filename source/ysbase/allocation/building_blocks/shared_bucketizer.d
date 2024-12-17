f50820621a0d5ce8aa9539f60a3119c6329acd72
Hazel Atkinson <yellowsink@riseup.net>
2024-12-17 14:27:29 +0000

    get shared bucketizer working

--- source/ysbase/allocation/building_blocks/shared_bucketizer.d
+++ source/ysbase/allocation/building_blocks/shared_bucketizer.d
@@ -5,23 +5,46 @@ License: $(LINK2 https://unlicense.org, Unlicense)
 +/
 module ysbase.allocation.building_blocks.shared_bucketizer;
 
-/// [HEAVILY WIP] std.experimental.bucketizer but `shared`, and requires `Allocator` to be shared
+/++
+A version of Phobos' `Bucketizer` but `shared`, and requires the `Allocator` to be shared.
+
+This allocator uses distinct allocator instances for each in a set of "buckets", so allocations with size in the range
+$(D [min, min + step$(RPAREN)) go to the first allocator, and $(D [min + step, min + step * 2$(RPAREN)) to the next bucket, and so on.
+
+Allocations directed to this allocator larger than `max` or smaller than `min` will fail safely.
+Try using $(LINK2 https://dlang.org/phobos/std_experimental_allocator_building_blocks_segregator.html, `Segregator`).
+
+$(SRCL ysbase/allocation/building_blocks/shared_bucketizer.d)
+
+Params:
+	Allocator =	The parent allocator to redirect allocation requests to. Must have `allocate() shared`.
+	min =			The minimum allocation size served by this allocator.
+	max =			The maximum allocation size served by this allocator.
+	step =		The size of the range of sizes to be directed to each bucket. Must be a factor of `max - min`.
+
+See_Also:
+$(LINK2 https://dlang.org/phobos/std_experimental_allocator_building_blocks_bucketizer.html, Phobos' `Bucketizer`)
++/
 shared struct SharedBucketizer(Allocator, size_t min, size_t max, size_t step)
 {
 		import ysbase.allocation.source_reexport : roundUpToMultipleOf, alignedAt;
 		import common = ysbase.allocation.source_reexport;
-		import std.traits : hasMember;
+		import std.traits : hasMember, hasFunctionAttributes;
 		import std.typecons : Ternary;
 
 		static assert((max - (min - 1)) % step == 0,
 				"Invalid limits when instantiating " ~ Bucketizer.stringof);
+
+		static assert(hasMember!(Allocator, "allocate")
+			&& hasFunctionAttributes!(Allocator.allocate, "shared"),
+				"Allocator must offer shared allocation");
 
 		// state
 		/**
 		The array of allocators is publicly available for e.g. initialization and
 		inspection.
 		*/
-		shared(Allocator)[(max + 1 - min) / step] buckets;
+		Allocator[(max + 1 - min) / step] buckets;
 
 		pure nothrow @safe @nogc
 		private shared(Allocator)* allocatorFor(size_t n)
@@ -30,14 +53,10 @@ shared struct SharedBucketizer(Allocator, size_t min, size_t max, size_t step)
 				return i < buckets.length ? &buckets[i] : null;
 		}
 
-		/**
-		The alignment offered is the same as `Allocator.alignment`.
-		*/
+		/// The alignment offered is the same as `Allocator.alignment`.
 		enum uint alignment = Allocator.alignment;
 
-		/**
-		Rounds up to the maximum size of the bucket in which `bytes` falls.
-		*/
+		/// Rounds up to the maximum size of the bucket in which `bytes` falls.
 		pure nothrow @safe @nogc
 		size_t goodAllocSize(size_t bytes) const
 		{
@@ -47,9 +66,7 @@ shared struct SharedBucketizer(Allocator, size_t min, size_t max, size_t step)
 				return min_1 + roundUpToMultipleOf(bytes - min_1, step);
 		}
 
-		/**
-		Directs the call to either one of the `buckets` allocators.
-		*/
+		/// Directs the call to either one of the `buckets` allocators.
 		void[] allocate(size_t bytes)
 		{
 				if (!bytes) return null;
@@ -62,8 +79,9 @@ shared struct SharedBucketizer(Allocator, size_t min, size_t max, size_t step)
 				return null;
 		}
 
+		/// Directs the call to either one of the `buckets` allocators.
 		static if (hasMember!(Allocator, "allocateZeroed"))
-		package(std) void[] allocateZeroed()(size_t bytes)
+		void[] allocateZeroed()(size_t bytes)
 		{
 				if (!bytes) return null;
 				if (auto a = allocatorFor(bytes))
@@ -96,7 +114,7 @@ shared struct SharedBucketizer(Allocator, size_t min, size_t max, size_t step)
 		/**
 		This method allows expansion within the respective bucket range. It succeeds
 		if both `b.length` and $(D b.length + delta) fall in a range of the form
-		$(D [min + k * step, min + (k + 1) * step - 1]).
+		$(D [min + k * step, min + (k + 1) * step$(RPAREN)).
 		*/
 		bool expand(ref void[] b, size_t delta)
 		{
@@ -112,7 +130,7 @@ shared struct SharedBucketizer(Allocator, size_t min, size_t max, size_t step)
 		/**
 		This method allows reallocation within the respective bucket range. If both
 		`b.length` and `size` fall in a range of the form $(D [min + k *
-		step, min + (k + 1) * step - 1]), then reallocation is in place. Otherwise,
+		step, min + (k + 1) * step$(RPAREN)), then reallocation is in place. Otherwise,
 		reallocation with moving is attempted.
 		*/
 		bool reallocate(ref void[] b, size_t size)
@@ -137,10 +155,7 @@ shared struct SharedBucketizer(Allocator, size_t min, size_t max, size_t step)
 				return common.reallocate(this, b, size);
 		}
 
-		/**
-		Similar to `reallocate`, with alignment. Defined only if `Allocator`
-		defines `alignedReallocate`.
-		*/
+		/// Similar to `reallocate`, with alignment. Defined only if `Allocator` defines `alignedReallocate`.
 		static if (hasMember!(Allocator, "alignedReallocate"))
 		bool alignedReallocate(ref void[] b, size_t size, uint a)
 		{
@@ -164,9 +179,7 @@ shared struct SharedBucketizer(Allocator, size_t min, size_t max, size_t step)
 				return common.alignedReallocate(this, b, size, a);
 		}
 
-		/**
-		Defined only if `Allocator` defines `owns`. Finds the owner of `b` and forwards the call to it.
-		*/
+		/// Defined only if `Allocator` defines `owns`. Finds the owner of `b` and forwards the call to it.
 		static if (hasMember!(Allocator, "owns"))
 		Ternary owns(void[] b)
 		{
@@ -179,9 +192,7 @@ shared struct SharedBucketizer(Allocator, size_t min, size_t max, size_t step)
 				return Ternary.no;
 		}
 
-		/**
-		This method is only defined if `Allocator` defines `deallocate`.
-		*/
+		/// This method is only defined if `Allocator` defines `deallocate`.
 		static if (hasMember!(Allocator, "deallocate"))
 		bool deallocate(void[] b)
 		{
@@ -225,10 +236,6 @@ shared struct SharedBucketizer(Allocator, size_t min, size_t max, size_t step)
 		}
 }
 
-// TODO: why is this not working when in a segregator?
-//enum isShared1 = is(typeof(SharedBucketizer!Mallocator) == shared);
-//enum isShared2 = !stateSize!(SharedBucketizer!Mallocator); // won't compile :(
-
 unittest
 {
 	import ysbase.allocation.building_blocks.shared_bucketizer;
@@ -246,8 +253,16 @@ unittest
 	alias a = SharedBucketizer!(SharedFreeList!(GCAllocator, 0, unbounded), 1, 128, 16);
 
 
-	shared Segregator!(8, a, a) b;
-	//shared SharedBucketizer!Mallocator b;
+	// TODO: need a shared segregator :(
+	//shared Segregator!(8, a, a) b;
+	a b;
 
-	//b.allocate(50);
+	import core.thread.osthread;
+
+	auto slic = b.allocate(50);
+
+	new Thread({
+		// shared!
+		b.deallocate(slic);
+	}).start().join();
 }
